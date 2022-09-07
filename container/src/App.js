120dc593b38e2f9e45ab7bdcbfa92017c2302cbc
Jason Lunsford <jason.lunsford@softvision.com>
2022-09-07 15:35:17 -0400

    source update

--- container/src/App.js
+++ container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
         <div>
-            <h1>Hello from Container!</h1>
+            <h1>Hello from Container</h1>
             <hr />
             <MarketingApp />
         </div>
