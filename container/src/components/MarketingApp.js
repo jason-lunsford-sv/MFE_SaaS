0a8468ac0c9fa0b514ddfd4f767ccd113df311d2
Jason Lunsford <jason.lunsford@softvision.com>
2022-09-08 10:04:48 -0400

    MFE navigation handoff implemented

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,30 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { mount } from 'marketing/MarketingApp';
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
+        // create marketing MFE here
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+
+                // verify the two paths are different to prevent an infinite loop
+                // of two routers trying to update each other
+                if (pathname !== nextPathname) {
+                    // update Browser History of container to reflect Memory Router state
+                    // of Marketing MFE
+                    history.push(nextPathname);
+                }
+            }
+        });
+
+        // ".listen()" is an event listener that will fire any function passed to it
+        history.listen(onParentNavigate);
     }, []);
 
     return (
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
-const mount = element => {
+const mount = (element, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         element
     );
+
+    // allows communication FROM container MFE down to Marketing MFE
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 };
 
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        // defaultHistory allows us to update the url path in the browser when the
+        // marketing MFE is ran in isolation
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
